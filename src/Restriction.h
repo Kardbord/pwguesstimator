cea2254e9d0d71b8ef93cb9ff6132079291888fd
Tanner Kvarfordt <tanner.kvarfordt@gmail.com>
2022-03-07 23:13:59 -0700

    Fixed alphanumeric regex

--- src/Restriction.h
+++ src/Restriction.h
@@ -79,7 +79,7 @@ namespace PasswordGuesstimator {
         'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
         'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
     };
-    static inline const std::regex ALPHANUMERIC_REGEX = std::regex("[a-zA-Z0-9]");
+    static inline const std::regex ALPHANUMERIC_REGEX = std::regex("[a-zA-Z0-9]+");
 
     inline auto character_set() -> const std::unordered_set<char> & override {
       return CHARACTER_SET;
